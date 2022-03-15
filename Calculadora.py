30baecf0582eb7e77fede2fedbf053dcf661d68a
Rodrigo de la Riva <100295171+Rodrigo-de-la-Riva@users.noreply.github.com>
2022-03-15 09:32:09 -0600

    archivo de python
    se creo el archivo de python en el priyecto

---
+++ Calculadora.py
@@ -0,0 +1,12 @@
+n1 = int(input("ingrese el primer numero: "))
+n2 = int(input("ingrese el primer numero: "))
+
+r1 = n1 + n2
+r2 = n1 + n2
+r3 = n1 + n2
+r4 = n1 + n2
+
+print("la suma de los numeros es de: ", r1)
+print("la resta de los numeros es de: ", r2)
+print("la multiplicacion de los numeros es de: ", r3)
+print("la division de los numeros es de: ", r4)
